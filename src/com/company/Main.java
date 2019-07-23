1cb663cd9290b867525a72d0f2c0db86791b44af
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-23 17:20:15 +0300

    print file from the files folder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,17 +10,20 @@ import java.nio.file.Path;
 public class Main {
 
     public static void main(String[] args) {
-	Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-	printFile(path);
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        printFile(path);
+        Path filePath = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
+        System.out.println("__________________________");
+        printFile(filePath);
     }
 
-    private static void printFile(Path path){
-        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+    private static void printFile(Path path) {
+        try (BufferedReader fileReader = Files.newBufferedReader(path)) {
             String line;
-            while ((line = fileReader.readLine()) != null){
+            while ((line = fileReader.readLine()) != null) {
                 System.out.println(line);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
