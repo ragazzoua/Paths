0726f8aec718a1c304c6f22f9d8ae8e0040a2c0d
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-23 17:17:15 +0300

    print file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+	printFile(path);
+    }
+
+    private static void printFile(Path path){
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = fileReader.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
