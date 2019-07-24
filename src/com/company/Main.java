61a7a2dd440bb5439d153dd3b9349d37bf159c4d
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 16:30:09 +0300

    view several methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,29 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
-            Files.createDirectories(dirToCreate);
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1/file1.txt");
+            long size = Files.size(filePath);
+            System.out.println("Size = " + size);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
 
+            BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attributes.size());
+            System.out.println("Last modified = " + attributes.lastModifiedTime());
+            System.out.println("Created = " + attributes.creationTime());
+            System.out.println("Is Directory = " + attributes.isDirectory());
+            System.out.println("Is Regular  = " + attributes.isRegularFile());
+
+
+
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
+//            Files.createDirectories(dirToCreate);
 
 //            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
 //            Files.createFile(fileToCreate);
