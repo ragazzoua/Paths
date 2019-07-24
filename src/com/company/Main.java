60993b13adde10adad7a7e560c2a78090059c1b5
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 13:57:38 +0300

    move file1copy.txt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,18 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.move(fileToMove, destination);
 
-            sourceFile = FileSystems.getDefault().getPath("Examples","Dir1");
-            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples","Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
