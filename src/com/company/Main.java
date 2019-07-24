f3ce85923d15abf6a3527480654199a357d76227
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 14:05:27 +0300

    delete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.move(fileToMove, destination);
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.delete(fileToDelete);
 
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.move(fileToMove, destination);
 
 //            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
 //            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
