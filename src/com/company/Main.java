1e66452c59eb3025f646e8fb96be3f32e7abcdc1
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 15:26:17 +0300

    create file and dir

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,22 @@
 package com.company;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.deleteIfExists(fileToDelete);
+//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.createFile(fileToCreate);
+            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+            Files.createDirectory(dirToCreate);
+
+//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.deleteIfExists(fileToDelete);
 
 
 //            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file2.txt");
