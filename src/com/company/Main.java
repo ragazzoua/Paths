d9d7b6f61c50d359cbc842e3e0356edd2d19d723
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 18:07:29 +0300

    use glob

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
             for (Path file : contents){
                 System.out.println(file.getFileName());
             }
