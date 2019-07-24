01af3015f651464993852fe8c94e37fddb49f660
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 18:19:52 +0300

    show only files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,17 @@ import java.nio.file.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path path) throws IOException {
+                return (Files.isRegularFile(path));
+            }
+        };
+
         Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
-            for (Path file : contents){
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
 
