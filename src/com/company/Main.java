d8f944a9849f7c8741c4235bf8124c3d4bdb0de4
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 17:21:38 +0300

    init read existing directory contents

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,60 +1,9 @@
 package com.company;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1/file1.txt");
-            long size = Files.size(filePath);
-            System.out.println("Size = " + size);
-            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
 
-            BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
-            System.out.println("Size = " + attributes.size());
-            System.out.println("Last modified = " + attributes.lastModifiedTime());
-            System.out.println("Created = " + attributes.creationTime());
-            System.out.println("Is Directory = " + attributes.isDirectory());
-            System.out.println("Is Regular  = " + attributes.isRegularFile());
-
-
-
-//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
-//            Files.createDirectories(dirToCreate);
-
-//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
-//            Files.createFile(fileToCreate);
-//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
-//            Files.createDirectory(dirToCreate);
-
-//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-//            Files.deleteIfExists(fileToDelete);
-
-
-//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file2.txt");
-//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Files.move(fileToMove, destination);
-
-//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-//            Files.move(fileToMove, destination);
-
-//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
-//
-//            sourceFile = FileSystems.getDefault().getPath("Examples","Dir1");
-//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
