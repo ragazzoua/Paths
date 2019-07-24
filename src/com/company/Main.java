7d58b0bfa95bca7d34eb1d7ae89643d3097585a5
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 17:55:43 +0300

    init read existing directory contents

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+            for (Path file : contents){
+                System.out.println(file.getFileName());
+            }
 
-
+        } catch (IOException | DirectoryIteratorException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
