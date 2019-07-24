bb43b497a892752bfed851cfbf0fd4b5a2ea90e3
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-24 13:37:38 +0300

    copyFile Example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,21 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
+import java.nio.file.CopyOption;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
