524ec10be04f142362898eee28ab60c99ac2445e
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-25 12:04:39 +0300

    printNames class created

---
+++ src/com/company/PrintNames.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class PrintNames extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println(dir.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+}
